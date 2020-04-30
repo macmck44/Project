fd2b16f01d129a81ac809cef2b4b78201d900a77
macmck44 <macmckenna97@gmail.com>
2020-04-30 15:49:14 +0100

    Updated PaymentDetails Activity

--- app/src/main/java/com/example/uuj/moviebuddy/PaymentDetails.java
+++ app/src/main/java/com/example/uuj/moviebuddy/PaymentDetails.java
@@ -1,8 +1,11 @@
 package com.example.uuj.moviebuddy;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by Owner on 29/04/2020.
@@ -22,5 +25,26 @@ public class PaymentDetails extends AppCompatActivity {
         tv_id = (TextView) findViewById(R.id.textid);
         tv_amount = (TextView) findViewById(R.id.textamount);
         tv_status = (TextView) findViewById(R.id.textstatus);
+
+        Intent intent = getIntent();
+
+        try {
+            JSONObject jsonObject = new JSONObject(intent.getStringExtra("PaymentDetails"));
+            showPayment(jsonObject.getJSONObject("response"), intent.getStringExtra("PaymentAmount"));
+
+        } catch (JSONException e){
+            e.printStackTrace();
+        }
+    }
+
+    private void showPayment(JSONObject response, String paymentAmount) {
+        try {
+            tv_id.setText(response.getString("id"));
+            tv_amount.setText(response.getString("state"));
+            tv_status.setText("£" + paymentAmount);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
